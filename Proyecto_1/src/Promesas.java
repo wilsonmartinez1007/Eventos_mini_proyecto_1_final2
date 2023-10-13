5a2644a516cbd7c49c4b57582dc94b6b9390b226
JuanCamacho1999 <144348942+JuanCamacho1999@users.noreply.github.com>
2023-10-12 23:54:35 -0500

    se arreglaron errores

--- Proyecto_1/src/Promesas.java
+++ Proyecto_1/src/Promesas.java
@@ -1,4 +1,4 @@
-public enum promesas {
+public enum Promesas {
     Reduccion_desempleo, Educacion_gratuita, Aumento_seguridad, Mejos_sistema_salud, Combatir_corrupcion, Planes_vivienda, Paz_total
 }
 
