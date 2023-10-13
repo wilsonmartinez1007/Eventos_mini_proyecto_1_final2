0779dd3d35f37f76024b5d081342b245df60ed29
wilsonmartinez1007 <117456018+wilsonmartinez1007@users.noreply.github.com>
2023-10-12 20:13:03 -0500

    caso1_

--- Proyecto_1/src/PartidoPolitico.java
+++ Proyecto_1/src/PartidoPolitico.java
@@ -1,3 +1,5 @@
 public enum PartidoPolitico {
-    
+    PartidoLiberalColombiano, PartidoConservadorColombiano, PartidoCambioRadical,
+    PartidoAlianzaVerde, Aico, PartidoAsi, PartidoPoliticoMira, PartidoDeLaU, PartidoPolo,
+    PartidoUP
 }
